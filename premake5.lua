d406486af7f86f37f3782fbf73e1510dac1f2c83
BaslOr <bastian.schwarzmann@web.de>
2026-02-21 15:21:27 +0100

    Added Premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+    cdialect "C11"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "Engine/vendor/GLFW/include/GLFW/**.h",
+        "Engine/vendor/GLFW/src/**.c"
+    }
+
+    includedirs {
+        "Engine/vendor/GLFW/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        defines { "_CRT_SECURE_NO_WARNINGS", "_GLFW_WIN32" }
+        links { "opengl32" }
+
+    filter "system:linux"
+        defines { "_GLFW_X11" }
+        links { "X11", "pthread", "dl", "GL" }
+
+    filter "system:macosx"
+        defines { "_GLFW_COCOA" }
+        links { "Cocoa.framework", "IOKit.framework", "CoreVideo.framework", "OpenGL.framework" }
+
+    filter {}
